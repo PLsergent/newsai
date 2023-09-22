b04dd678599e84544f5578995ecba89f5488a6c7
PL Sergent <papa.louis59@gmail.com>
2023-09-22 20:59:13 +0200

    Component List WIP

---
+++ tailwind.config.js
@@ -0,0 +1,6 @@
+/** @type {import('tailwindcss').Config} */
+export const content = [
+    './src/pages/**/*.{html,js,astro}',
+    './src/components/**/*.{html,js,astro}',
+    './src/layouts/**/*.{html,js,astro}'
+];
